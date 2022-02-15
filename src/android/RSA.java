7dcc31aff4487789b20adb5aaa3ac69abba69db0
Alexey <ermashov.a.a@gmail.com>
2022-02-15 12:40:26 +0300

    code fix

--- src/android/RSA.java
+++ src/android/RSA.java
@@ -1,12 +1,12 @@
 package ru.rsa.plugins;
 
 import android.content.Context;
+import android.security.keystore.KeyProperties;
 import android.util.Base64;
 import android.util.Log;
 
 import android.security.KeyPairGeneratorSpec;
 
-import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaCertStore;
 import org.bouncycastle.cms.CMSAlgorithm;
 import org.bouncycastle.cms.CMSEnvelopedData;
@@ -24,7 +24,6 @@ import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
 import org.bouncycastle.cms.jcajce.JceKeyTransRecipientInfoGenerator;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
-import org.bouncycastle.operator.DigestCalculatorProvider;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
 import org.bouncycastle.util.Store;
@@ -39,7 +38,6 @@ import java.security.PrivateKey;
 import java.security.Security;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.security.interfaces.RSAPrivateKey;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -52,7 +50,6 @@ import java.util.List;
 
 public class RSA {
     private static final String KEYSTORE_PROVIDER = "AndroidKeyStore";
-    private static final Cipher CIPHER = getCipher();
 
     public static void createKeyPair(Context ctx,  String alias) throws Exception {
         Calendar notBefore = Calendar.getInstance();
@@ -69,7 +66,7 @@ public class RSA {
             .setEndDate(notAfter.getTime())
             .setEncryptionRequired()
             .setKeySize(2048)
-            .setKeyType("RSA")
+            .setKeyType(KeyProperties.KEY_ALGORITHM_RSA)//"RSA"
             .build();
         KeyPairGenerator kpGenerator = KeyPairGenerator.getInstance("RSA", KEYSTORE_PROVIDER);
         kpGenerator.initialize(spec);
@@ -78,7 +75,14 @@ public class RSA {
 
     public static boolean isEntryAvailable(String alias) {
         try {
-            return loadKey(Cipher.ENCRYPT_MODE, alias) != null;
+            KeyStore keyStore = KeyStore.getInstance(KEYSTORE_PROVIDER);
+            keyStore.load(null, null);
+            Certificate cert = keyStore.getCertificate(alias);
+
+            if(cert == null)
+                return false;
+            else
+                return true;
         } catch (Exception e) {
             return false;
         }
@@ -91,39 +95,19 @@ public class RSA {
             keyStore.deleteEntry(alias);
             return true;
         }  catch (Exception e) {
-            Log.v("Error", e.getMessage());
+            //Log.v("Error", e.getMessage());
             return true;
         }
-    }
-
-    private static Key loadKey(int cipherMode, String alias) throws Exception {
-        KeyStore keyStore = KeyStore.getInstance(KEYSTORE_PROVIDER);
-        keyStore.load(null, null);
-        Key key;
-
-        switch (cipherMode) {
-            case 0:
-            case Cipher.ENCRYPT_MODE:
-                key = keyStore.getCertificate(alias).getPublicKey();
-                if (key == null) {
-                    throw new Exception("Failed to load the public key for " + alias);
-                }
-                break;
-            case  Cipher.DECRYPT_MODE:
-                key = keyStore.getKey(alias, null);
-                if (key == null) {
-                    throw new Exception("Failed to load the private key for " + alias);
-                }
-                break;
-            default : throw new Exception("Invalid cipher mode parameter");
-        }
-        return key;
     }
 
     public static String getCertificate(String alias) throws Exception{
         KeyStore keyStore = KeyStore.getInstance(KEYSTORE_PROVIDER);
         keyStore.load(null, null);
         Certificate cert = keyStore.getCertificate(alias);
+
+        if(cert == null)
+            throw new Exception("Certificate and key pair doesn't exist");
+
         return Base64.encodeToString(cert.getEncoded(), Base64.NO_WRAP);
     }
 
@@ -144,98 +128,26 @@ public class RSA {
 
         Certificate cert = keyStore.getCertificate(alias);
 
-
-
-        Log.v("RSAPlugin", "cmsSign 2.0.1");
-
         KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) keyStore.getEntry(alias, null);
         PrivateKey privateKey = privateKeyEntry.getPrivateKey();
 
-        Log.v("RSAPlugin", "cmsSign 2.0");
-        //PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, null);
-        //try {
+        byte[] signedMessage = null;
+        List<X509Certificate> certList = new ArrayList<X509Certificate>();
+        CMSTypedData cmsData= new CMSProcessableByteArray(data.getBytes());
+        certList.add((X509Certificate) cert);
+        Store certs = new JcaCertStore(certList);
+        CMSSignedDataGenerator cmsGenerator = new CMSSignedDataGenerator();
+        ContentSigner contentSigner= new JcaContentSignerBuilder("SHA256withRSA").build(privateKey);
+        cmsGenerator.addSignerInfoGenerator(new JcaSignerInfoGeneratorBuilder(
+                new JcaDigestCalculatorProviderBuilder().setProvider("BC")
+                        .build()).build(contentSigner, (X509Certificate) cert));
+        cmsGenerator.addCertificates(certs);
 
-
-
-
-
-            byte[] signedMessage = null;
-            List<X509Certificate> certList = new ArrayList<X509Certificate>();
-            CMSTypedData cmsData= new CMSProcessableByteArray(data.getBytes());
-            certList.add((X509Certificate) cert);
-            Store certs = new JcaCertStore(certList);
-            CMSSignedDataGenerator cmsGenerator = new CMSSignedDataGenerator();
-            ContentSigner contentSigner= new JcaContentSignerBuilder("SHA256withRSA").build(privateKey);
-            cmsGenerator.addSignerInfoGenerator(new JcaSignerInfoGeneratorBuilder(
-                    new JcaDigestCalculatorProviderBuilder().setProvider("BC")
-                            .build()).build(contentSigner, (X509Certificate) cert));
-            cmsGenerator.addCertificates(certs);
-
-            CMSSignedData cms = cmsGenerator.generate(cmsData, true);
-            signedMessage = cms.getEncoded();
-
-            Log.v("RSAPlugin signedMessage", Base64.encodeToString(signedMessage, Base64.NO_WRAP));
+        CMSSignedData cms = cmsGenerator.generate(cmsData, true);
+        signedMessage = cms.getEncoded();
 
 
         return Base64.encodeToString(signedMessage, Base64.NO_WRAP);
-
-        //RSAPrivateKey privateKey = (RSAPrivateKey) privateKeyEntry.getPrivateKey();
-
-/*
-        Log.v("RSAPlugin", "cmsSign 2");
-
-        List certList = new ArrayList();
-        CMSTypedData msg = new CMSProcessableByteArray(data.getBytes());
-
-        Log.v("RSAPlugin", "cmsSign 3");
-
-        certList.add(cert);
-
-        Log.v("RSAPlugin", "cmsSign 3.1");
-
-        Store certs = new JcaCertStore(certList);
-
-        Log.v("RSAPlugin", "cmsSign 3.2");
-
-
-        CMSSignedDataGenerator gen = new CMSSignedDataGenerator();
-
-        Log.v("RSAPlugin", "cmsSign 3.3");
-
-        try {
-
-
-
-              ContentSigner sha1Signer = new JcaContentSignerBuilder("SHA256withRSA")
-                .setProvider("BC")
-                .build(privateKey);
-
-            Log.v("RSAPlugin", "cmsSign 3.4");
-
-
-            gen.addSignerInfoGenerator(
-                    new JcaSignerInfoGeneratorBuilder(
-                            new JcaDigestCalculatorProviderBuilder().setProvider("BC").build())
-                            .build(sha1Signer, (X509Certificate) cert));
-
-            Log.v("RSAPlugin", "cmsSign 3.5");
-
-            gen.addCertificates(certs);
-
-            Log.v("RSAPlugin", "cmsSign 4");
-
-            CMSSignedData sigData = gen.generate(msg, true);
-
-            Log.v("RSAPlugin", "cmsSign 5");
-
-            return Base64.encodeToString(sigData.getEncoded(), Base64.NO_WRAP);
-
-        } catch (Exception e) {
-            Log.v("RSAPlugin", e.getMessage());
-        }*/
-
-
-       // return "";
     }
 
     public static String cmsEncrypt(String certs, String data) throws Exception{
@@ -254,37 +166,16 @@ public class RSA {
                 new JceCMSContentEncryptorBuilder(CMSAlgorithm.DES_CBC)
                         .setProvider(BouncyCastleProvider.PROVIDER_NAME).build());
 
-        //Log.v("RSAPlugin encrypt", Base64.encodeToString(cmsEnvelopedData.getEncoded(), Base64.NO_WRAP));
-
         return Base64.encodeToString(cmsEnvelopedData.getEncoded(), Base64.NO_WRAP);
     }
 
     public static String cmsDecrypt(String alias, String data) throws Exception{
-
         Security.addProvider(new BouncyCastleProvider());
-        //try {
         CMSEnvelopedDataParser envDataParser = new CMSEnvelopedDataParser(Base64.decode(data, Base64.DEFAULT));
         RecipientInformationStore recipients =  envDataParser.getRecipientInfos();
         RecipientInformation recipient = (RecipientInformation) recipients.getRecipients().iterator().next();
         PrivateKey privateKey = RSA.getPrivateKey( alias);
         byte[] envelopedData = recipient.getContent(new JceKeyTransEnvelopedRecipient(privateKey));
-
-        //String pDataBase64  =  new String(envelopedData, StandardCharsets.UTF_8);
-        //Log.v("RSAPlugin d base64",  pDataBase64);
-
-        //String pData = new String(Base64.decode(envelopedData, Base64.DEFAULT), StandardCharsets.UTF_8);
-        //Log.v("RSAPlugin decrypt",  pData);
-
         return new String(envelopedData, StandardCharsets.UTF_8);
-    }
-
-    private static Cipher getCipher() {
-        try {
-           // return Cipher.getInstance("DES/CBC/PKCS5Padding");
-           return Cipher.getInstance("RSA/ECB/PKCS1Padding");
-
-        } catch (Exception e) {
-            return null;
-        }
     }
 }
--- src/android/RSAPlugin.java
+++ src/android/RSAPlugin.java
@@ -1,63 +1,18 @@
 package ru.rsa.plugins;
 
-
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
-import org.bouncycastle.cert.jcajce.JcaCertStore;
-import org.bouncycastle.cms.CMSAlgorithm;
-import org.bouncycastle.cms.CMSEnvelopedData;
-import org.bouncycastle.cms.CMSEnvelopedDataGenerator;
-import org.bouncycastle.cms.CMSEnvelopedDataParser;
-import org.bouncycastle.cms.CMSProcessableByteArray;
-import org.bouncycastle.cms.CMSSignedData;
-import org.bouncycastle.cms.CMSSignedDataGenerator;
-import org.bouncycastle.cms.CMSTypedData;
-import org.bouncycastle.cms.RecipientInformation;
-import org.bouncycastle.cms.RecipientInformationStore;
-import org.bouncycastle.cms.SignerInfoGeneratorBuilder;
-import org.bouncycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder;
-import org.bouncycastle.cms.jcajce.JceCMSContentEncryptorBuilder;
-import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
-import org.bouncycastle.cms.jcajce.JceKeyTransRecipientInfoGenerator;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.operator.ContentSigner;
-import org.bouncycastle.operator.DigestCalculatorProvider;
-import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
-import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
-import org.bouncycastle.util.Store;
 import org.json.JSONArray;
-
-import android.content.Context;
-import android.util.Base64;
 import android.util.Log;
 
-import com.sun.jna.NativeLong;
-
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
-import java.security.PrivateKey;
-import java.security.Security;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-
 public class RSAPlugin extends CordovaPlugin {
-
 
     public RSAPlugin() {}
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
 
-        Context context = this.cordova.getActivity().getApplicationContext();
-
-        if (action.equals("init"))
-        {
+        if (action.equals("init")) {
             cordova.getThreadPool().execute(new Runnable() {
                 @Override
                 public void run() {
@@ -66,80 +21,55 @@ public class RSAPlugin extends CordovaPlugin {
                         if (!RSA.isEntryAvailable(alias)) {
                             RSA.createKeyPair(cordova.getActivity().getApplicationContext(), alias);
                         }
-
                         String certificate = RSA.getCertificate(alias);
-
                         PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, certificate);
                         callbackContext.sendPluginResult(pluginResult);
-
                     } catch (Exception e) {
-                        callbackContext.error("token error ex.");
-
+                        //Log.v("certificate", e.getMessage());
+                        callbackContext.error(e.getMessage());
                     }
-
                 }
             });
-
             return true;
-
-        }else if (action.equals("getCertificate"))
-        {
-
+        } else if (action.equals("getCertificate")) {
             cordova.getThreadPool().execute(new Runnable() {
                 @Override
                 public void run() {
                     try {
-
                         String alias = args.getString(0);
-
                         String certificate = RSA.getCertificate(alias);
-
                         PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, certificate);
                         callbackContext.sendPluginResult(pluginResult);
-
                     } catch (Exception e) {
-                        callbackContext.error("token error ex.");
-
+                        callbackContext.error(e.getMessage());
                     }
-
                 }
             });
-
             return true;
-
-        }else if (action.equals("remove"))
-        {
-
+        } else if (action.equals("remove")) {
             cordova.getThreadPool().execute(new Runnable() {
                 @Override
                 public void run() {
                     try {
-
                         String alias = args.getString(0);
-
-                        String certificate = RSA.deleteKeyPair(alias);
-
-                        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "ok");
-                        callbackContext.sendPluginResult(pluginResult);
-
+                      if (RSA.deleteKeyPair(alias)) {
+                          PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "ok");
+                          callbackContext.sendPluginResult(pluginResult);
+                        }else{
+                          PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "ok");
+                          callbackContext.sendPluginResult(pluginResult);
+                        }
                     } catch (Exception e) {
-                        callbackContext.error("token error ex.");
-
+                        callbackContext.error(e.getMessage());
                     }
-
                 }
             });
-
             return true;
-
-        }
-        else if(action.equals("cmsSign"))
-        {
+        } else if(action.equals("cmsSign")) {
             cordova.getThreadPool().execute(new Runnable() {
                 @Override
                 public void run() {
                     try {
-
                         String alias = args.getString(0);
                         String pData = args.getString(1);
 
@@ -147,18 +77,13 @@ public class RSAPlugin extends CordovaPlugin {
 
                         PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, cmsSing);
                         callbackContext.sendPluginResult(pluginResult);
-
                     }catch (Exception e){
                         callbackContext.error(e.getMessage());
                     }
                 }
-
             });
             return true;
-
-        }
-        else if(action.equals("cmsEncrypt"))
-        {
+        } else if(action.equals("cmsEncrypt")) {
             cordova.getThreadPool().execute(new Runnable() {
                 @Override
                 public void run() {
@@ -172,20 +97,14 @@ public class RSAPlugin extends CordovaPlugin {
                         PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, encryptData);
                         pluginResult.setKeepCallback(true);
                         callbackContext.sendPluginResult(pluginResult);
-
                     }catch (Exception e){
                         callbackContext.error(e.getMessage());
 
                     }
-
                 }
             });
-
             return true;
-
-        }
-        else if(action.equals("cmsDecrypt"))
-        {
+        } else if(action.equals("cmsDecrypt")) {
             cordova.getThreadPool().execute(new Runnable() {
                 @Override
                 public void run() {
@@ -197,16 +116,13 @@ public class RSAPlugin extends CordovaPlugin {
 
                         PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, decryptData);
                         callbackContext.sendPluginResult(pluginResult);
-
                     }catch (Exception e){
-                        Log.v("Error", e.getMessage());
+                       // Log.v("Error", e.getMessage());
                         callbackContext.error(e.getMessage());
                     }
                 }
-
             });
             return true;
-
         }
 
         callbackContext.error("method not found");
